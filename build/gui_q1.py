5c893731650f507b220f1bb8f8b5a371357b270d
Regina <31191580+reginaceballos@users.noreply.github.com>
2024-04-30 14:18:44 -0400

    Working video recorder for q1

--- build/gui_q1.py
+++ build/gui_q1.py
@@ -2,9 +2,9 @@ from utils_gui import *
 import tkinter as tk
 from tkinter import Canvas, PhotoImage, Label, Button, Frame
 import threading
-#from video_recorder import *
 import cv2
 from PIL import Image, ImageTk
+# import multiprocessing
 
 
 width = 479
@@ -233,65 +233,42 @@ def create_gui_q1(window, next_frame, next_first_ask_function):
     image_frame_q1.place(
         x=829,
         y=182,
-        bd=0
     )
 
-    image_video_q1 = Label(image_frame_q1, width=width, height=height)
+    image_video_q1 = Label(image_frame_q1, width=width, height=height,
+        bd=0)
     image_video_q1.place(
         x=0,
-        y=0,
-        bd=0
+        y=0
     )
-
-
     
 
     return frame_q1
 
-record = False
+recording = False
 
 
 def recording_button(file_suffix):
-    global record, \
+    global recording, \
         button_repeat_q1, button_repeat_image_q1, button_repeat_image_hover_q1,\
         button_record_q1, button_record_image_q1, button_record_image_hover_q1, \
         button_play_q1, button_play_image_q1, button_play_image_hover_q1, \
         button_next_q1, button_next_image_q1, button_next_image_hover_q1, \
-        cap_play_q1
+        cap_play_q1, thread
 
-    if record:
-        # thread_1 = threading.Thread(target=stop)
-        # thread_2 = threading.Thread(target=start_recording_proc)
-        # thread_1.start()
-        # thread_2.start()
-        stop()
-        stop_frame()
+    if not recording:
 
-        button_record_image_q1 = PhotoImage(file=relative_to_assets("button_record.png"))
-        button_record_image_hover_q1 = PhotoImage(file=relative_to_assets("button_record_hover.png"))
-        button_record_q1["image"] = button_record_image_hover_q1
+        recording = not recording
 
-        button_repeat_image_q1 = PhotoImage(file=relative_to_assets("button_repeat.png"))
-        button_repeat_image_hover_q1 = PhotoImage(file=relative_to_assets("button_repeat_hover.png"))
-        button_repeat_q1["image"] = button_repeat_image_q1
-        button_repeat_q1["state"] = "normal"
-
-        button_play_image_q1 = PhotoImage(file=relative_to_assets("button_play.png"))
-        button_play_image_hover_q1 = PhotoImage(file=relative_to_assets("button_play_hover.png"))
-        button_play_q1["image"] = button_play_image_q1
-        button_play_q1["state"] = "normal"
-
-        button_next_image_q1 = PhotoImage(file=relative_to_assets("button_next.png"))
-        button_next_image_hover_q1 = PhotoImage(file=relative_to_assets("button_next_hover.png"))
-        button_next_q1["image"] = button_next_image_q1
-        button_next_q1["state"] = "normal"
-
-    else:
-        start(file_suffix)
+        start_audio(file_suffix)
 
         cap_play_q1 = cv2.VideoCapture(0)
         
-        show_frame()
+        show_frame_q1()
+
+        thread = threading.Thread(target=lambda: start_recording_video('q1'))
+        thread.start()
+        
 
         button_record_image_q1 = PhotoImage(file=relative_to_assets("button_stop_recording.png"))
         button_record_image_hover_q1 = PhotoImage(file=relative_to_assets("button_stop_recording_hover.png"))
@@ -312,7 +289,30 @@ def recording_button(file_suffix):
         button_next_q1["image"] = button_next_image_q1
         button_next_q1["state"] = "disabled"
 
-    record = not record
+
+    else:
+        recording = not recording
+        stop_audio()
+        stop_frame_q1()
+
+        button_record_image_q1 = PhotoImage(file=relative_to_assets("button_record.png"))
+        button_record_image_hover_q1 = PhotoImage(file=relative_to_assets("button_record_hover.png"))
+        button_record_q1["image"] = button_record_image_hover_q1
+
+        button_repeat_image_q1 = PhotoImage(file=relative_to_assets("button_repeat.png"))
+        button_repeat_image_hover_q1 = PhotoImage(file=relative_to_assets("button_repeat_hover.png"))
+        button_repeat_q1["image"] = button_repeat_image_q1
+        button_repeat_q1["state"] = "normal"
+
+        button_play_image_q1 = PhotoImage(file=relative_to_assets("button_play.png"))
+        button_play_image_hover_q1 = PhotoImage(file=relative_to_assets("button_play_hover.png"))
+        button_play_q1["image"] = button_play_image_q1
+        button_play_q1["state"] = "normal"
+
+        button_next_image_q1 = PhotoImage(file=relative_to_assets("button_next.png"))
+        button_next_image_hover_q1 = PhotoImage(file=relative_to_assets("button_next_hover.png"))
+        button_next_q1["image"] = button_next_image_q1
+        button_next_q1["state"] = "normal"
 
 
 
@@ -336,7 +336,7 @@ def first_ask_q1():
     button_record_q1["state"] = "normal"
 
 
-def show_frame():
+def show_frame_q1():
     ret_play, frame_play = cap_play_q1.read()
 
     if ret_play:
@@ -348,11 +348,36 @@ def show_frame():
 
         image_video_q1.imgtk = imgtk
         image_video_q1.configure(image=imgtk)
-        image_video_q1.after(10, show_frame)
+        image_video_q1.after(10, show_frame_q1)
 
 
-def stop_frame():
+def stop_frame_q1():
     if cap_play_q1.isOpened():
         cap_play_q1.release()
 
     image_video_q1.configure(image=image_file_no_video_display_q1)
+
+
+def start_recording_video(file_suffix):
+    global recording
+    
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('answer_questions_video/video_answer_' + file_suffix + '.mp4',fourcc,  25.0, (1280, 720))
+
+    cap_record = cv2.VideoCapture(0)
+
+    if not cap_record.isOpened():
+        print("Error: Could not open camera.")
+        return
+
+
+    while recording:
+        ret_record, frame_record = cap_record.read()
+        
+        if ret_record:
+            out.write(frame_record)
+        else:
+            break
+
+    cap_record.release()
+    out.release()
--- test.py
+++ test.py
@@ -77,55 +77,55 @@
 
 
 
-# import multiprocessing
-# import tkinter as tk
-# import cv2
+import multiprocessing
+import tkinter as tk
+import cv2
 
-# e = multiprocessing.Event()
-# p = None
+e = multiprocessing.Event()
+p = None
 
-# # -------begin capturing and saving video
-# def startrecording(e):
-#     cap = cv2.VideoCapture(0)
-#     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#     out = cv2.VideoWriter('output.mp4',fourcc,  25.0, (1280, 720))
+# -------begin capturing and saving video
+def startrecording(e):
+    cap = cv2.VideoCapture(0)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.mp4',fourcc,  25.0, (1280, 720))
 
-#     while(cap.isOpened()):
-#         if e.is_set():
-#             cap.release()
-#             out.release()
-#             cv2.destroyAllWindows()
-#             e.clear()
-#         ret, frame = cap.read()
-#         if ret==True:
-#             out.write(frame)
-#         else:
-#             break
+    while(cap.isOpened()):
+        if e.is_set():
+            cap.release()
+            out.release()
+            cv2.destroyAllWindows()
+            e.clear()
+        ret, frame = cap.read()
+        if ret==True:
+            out.write(frame)
+        else:
+            break
 
-# def start_recording_proc():
-#     global p
-#     p = multiprocessing.Process(target=startrecording, args=(e,))
-#     p.start()
+def start_recording_proc():
+    global p
+    p = multiprocessing.Process(target=startrecording, args=(e,))
+    p.start()
 
-# # -------end video capture and stop tk
-# def stoprecording():
-#     e.set()
-#     p.join()
+# -------end video capture and stop tk
+def stoprecording():
+    e.set()
+    p.join()
 
-#     root.quit()
-#     root.destroy()
+    root.quit()
+    root.destroy()
 
-# if __name__ == "__main__":
-#     # -------configure window
-#     root = tk.Tk()
-#     root.geometry("%dx%d+0+0" % (100, 100))
-#     startbutton=tk.Button(root,width=10,height=1,text='START',command=start_recording_proc)
-#     stopbutton=tk.Button(root,width=10,height=1,text='STOP', command=stoprecording)
-#     startbutton.pack()
-#     stopbutton.pack()
+if __name__ == "__main__":
+    # -------configure window
+    root = tk.Tk()
+    root.geometry("%dx%d+0+0" % (100, 100))
+    startbutton=tk.Button(root,width=10,height=1,text='START',command=start_recording_proc)
+    stopbutton=tk.Button(root,width=10,height=1,text='STOP', command=stoprecording)
+    startbutton.pack()
+    stopbutton.pack()
 
-#     # -------begin
-#     root.mainloop()
+    # -------begin
+    root.mainloop()
 
 
 
@@ -356,40 +356,40 @@
 
 
 
-import numpy as np
-import cv2
-import tkinter as tk
-from PIL import Image, ImageTk
+# import numpy as np
+# import cv2
+# import tkinter as tk
+# from PIL import Image, ImageTk
 
-#Set up GUI
-window = tk.Tk()  #Makes main window
-window.wm_title("Digital Microscope")
-window.config(background="#FFFFFF")
+# #Set up GUI
+# window = tk.Tk()  #Makes main window
+# window.wm_title("Digital Microscope")
+# window.config(background="#FFFFFF")
 
-#Graphics window
-imageFrame = tk.Frame(window, width=600, height=500)
-imageFrame.grid(row=0, column=0, padx=10, pady=2)
+# #Graphics window
+# imageFrame = tk.Frame(window, width=600, height=500)
+# imageFrame.grid(row=0, column=0, padx=10, pady=2)
 
-#Capture video frames
-lmain = tk.Label(imageFrame)
-lmain.grid(row=0, column=0)
-cap = cv2.VideoCapture(0)
-def show_frame():
-    _, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-    img = Image.fromarray(cv2image)
-    imgtk = ImageTk.PhotoImage(image=img)
-    lmain.imgtk = imgtk
-    lmain.configure(image=imgtk)
-    lmain.after(10, show_frame) 
-
-
-
-#Slider window (slider controls stage position)
-sliderFrame = tk.Frame(window, width=600, height=100)
-sliderFrame.grid(row = 600, column=0, padx=10, pady=2)
+# #Capture video frames
+# lmain = tk.Label(imageFrame)
+# lmain.grid(row=0, column=0)
+# cap = cv2.VideoCapture(0)
+# def show_frame():
+#     _, frame = cap.read()
+#     frame = cv2.flip(frame, 1)
+#     cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+#     img = Image.fromarray(cv2image)
+#     imgtk = ImageTk.PhotoImage(image=img)
+#     lmain.imgtk = imgtk
+#     lmain.configure(image=imgtk)
+#     lmain.after(10, show_frame) 
 
 
-show_frame()  #Display 2
-window.mainloop()  #Starts GUI
+
+# #Slider window (slider controls stage position)
+# sliderFrame = tk.Frame(window, width=600, height=100)
+# sliderFrame.grid(row = 600, column=0, padx=10, pady=2)
+
+
+# show_frame()  #Display 2
+# window.mainloop()  #Starts GUI
